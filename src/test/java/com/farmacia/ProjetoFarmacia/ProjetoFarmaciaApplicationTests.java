af9e1746ee38635acce46049805f5572f0c80b3e
let-taina <le_taina@hotmail.com>
2024-06-10 13:27:31 -0300

    Início do projeto c config

---
+++ src/test/java/com/farmacia/ProjetoFarmacia/ProjetoFarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmacia.ProjetoFarmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
